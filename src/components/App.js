6cb1e8e2be959b81c1d0b6b44066dcf7cce8e126
Joshua Richardson <joshua.richardson11@icloud.com>
2020-07-28 19:35:11 -0700

    Final version

--- src/components/App.js
+++ src/components/App.js
@@ -32,9 +32,6 @@ const App = () => {
       showAlert("Please enter all fields", "danger");
       return false;
     }
-    // item._id = Math.floor(Math.random() * 90000) + 10000;
-    // item.created = new Date().toString();
-    // setLogs([...logs, item]);
 
     ipcRenderer.send("logs:add", item);
     showAlert("Log Added");
